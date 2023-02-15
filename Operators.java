69ad7f958d9d014566d505e7031d23741bc81980
deepak76901 <deepakb76901@gmail.com>
2023-02-15 19:44:52 +0530

    Operators ko commit kr riya hu

---
+++ Operators.java
@@ -0,0 +1,32 @@
+
+public class Operators {
+    public static void main(String []args) {
+        // Assignment Operators
+
+        // int a = 10;
+        // int b= 20;
+        // a /= 5;
+        // System.out.println(a);
+        // b += 10;
+        // System.out.println(b);
+        
+        
+        
+        // Relational Operrators
+        
+        // int a=10;
+        // int b=10;
+        // int c=30;
+        // boolean res = (a!=c);
+        // System.out.println(res);
+        
+        // Logical Operrators 
+        
+         int a=10;
+         int b=20;
+         int c=30;
+        System.out.println(a>=b || a==c || a>=b);
+    }
+    
+    
+}
---
+++ TypeCastingAndTruncation.java
@@ -0,0 +1,14 @@
+public class TypeCastingAndTruncation {
+    public static void main(String []args) {
+        int num1  ;
+        double num2 = 10;
+        num1 = (int)num2;
+        System.out.println(num1); 
+        
+        int n1=12;
+        int n2 = 5;
+        float res  = n1/n2;
+        System.out.println(res);
+        
+    }
+}
